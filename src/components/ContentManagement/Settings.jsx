e26de43d2ed16f3b188d11ed0266ad2235efc71a
eduardo-dangelo <eduardodonatodangelo@gmail.com>
2018-05-26 10:19:47 +0100

    GRAPHCOOL IMPLEMENTATION

--- src/components/ContentManagement/Settings.jsx
+++ src/components/ContentManagement/Settings.jsx
@@ -3,9 +3,45 @@ import FaCog from 'react-icons/lib/fa/cog'
 import {Col, Row} from "react-bootstrap"
 import Button from "../Button"
 import FieldGroup from '../FormControl'
+import Dropzone from 'react-dropzone'
+import axios, { post } from 'axios'
 
 class Settings extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      file: null,
+    };
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.fileUpload = this.fileUpload.bind(this);
+  }
+
+  onFormSubmit(e) {
+    e.preventDefault();
+    this.fileUpload(this.state.file);
+  }
+
+  onChange(e) {
+    this.setState({ file: e.target.files[0] });
+  }
+
+  fileUpload(file) {
+    let data = new FormData();
+    data.append('data', file);
+    axios
+      .post(`https://api.graph.cool/file/v1/cjhfdhnwc1gyg0196jr0feqnf`, data, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+      .then(response => {
+        console.log('file upload response', response);
+      });
+  }
+
   render() {
+    console.log('this.state.files', this.state.files)
     return (
       <div className="container admin-container">
         <div className="admin-box animated bounceInUp">
@@ -66,7 +102,22 @@ class Settings extends React.Component {
                       label={'Save Changes'}
                     />
                   </Col>
+                  {/*<Col sm={12}>*/}
+                    {/*<Button*/}
+                      {/*type={'button'}*/}
+                      {/*label={'Upload'}*/}
+                      {/*handleClick={this.uploadFile}*/}
+                    {/*/>*/}
+                    {/*<Dropzone onDrop={this.onDrop.bind(this)}>*/}
+                      {/*<p>Try dropping some files here, or click to select files to upload.</p>*/}
+                    {/*</Dropzone>*/}
+                  {/*</Col>*/}
                 </Row>
+              </form>
+              <form onSubmit={this.onFormSubmit}>
+                <h1>File Upload</h1>
+                <input type="file" onChange={this.onChange} />
+                <button type="submit">Upload</button>
               </form>
             </Col>
             </Col>
--- src/index.js
+++ src/index.js
@@ -3,15 +3,42 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './components/App/App'
 import ContentManagement from './components/ContentManagement'
-import { BrowserRouter as Router, Route } from "react-router-dom"
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { ApolloProvider  } from 'react-apollo'
+import ApolloClient from 'apollo-client';
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 import registerServiceWorker from './registerServiceWorker'
+import { SubscriptionClient, addGraphQLSubscriptions } from 'subscriptions-transport-ws'
+import * as WebSocket from 'isomorphic-ws'
+
+// Simple API:        https://api.graph.cool/simple/v1/cjhfdhnwc1gyg0196jr0feqnf
+// Relay API:         https://api.graph.cool/relay/v1/cjhfdhnwc1gyg0196jr0feqnf
+// Subscriptions API: wss://subscriptions.graph.cool/v1/cjhfdhnwc1gyg0196jr0feqnf
+
+const httpLink = new HttpLink({ uri: 'https://api.graph.cool/simple/v1/cjhfdhnwc1gyg0196jr0feqnf' })
+
+const wsClient = new SubscriptionClient('wss://subscriptions.graph.cool/v1/cjhfdhnwc1gyg0196jr0feqnf', {
+  reconnect: true,
+  timeout: 20000
+}, WebSocket)
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+  dataIdFromObject: o => o.id,
+  wsClient
+})
+
 
 ReactDOM.render(
-  <Router>
-    <div>
-      <Route exact path="/" component={App}/>
-      <Route exact path="/admin" component={ContentManagement}/>
-    </div>
-  </Router>
-  , document.getElementById('root'));
+  <ApolloProvider client={client}>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={App}/>
+        <Route exact path="/admin" component={ContentManagement}/>
+      </Switch>
+    </Router>
+  </ApolloProvider>,
+  document.getElementById('root'));
 registerServiceWorker();
